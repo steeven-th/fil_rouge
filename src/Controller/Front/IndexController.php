e1907266057cde3603be402d607e42c7395c76a5
Steeven <steeventhomas64@gmail.com>
2021-12-04 11:18:03 +0100

    Replace CSS by SCSS

--- src/Controller/Front/IndexController.php
+++ src/Controller/Front/IndexController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class FrontController extends AbstractController {
+class IndexController extends AbstractController {
     /**
      * @Route("/", name="index")
      */
     public function index(): Response {
         return $this->render('front/index.html.twig', [
-            'controller_name' => 'FrontController',
+            'controller_name' => 'IndexController',
         ]);
     }
 }
