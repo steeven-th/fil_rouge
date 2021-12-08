95a7444f4385bba00ef2d8b13dc36f07368338a2
Steeven <steeventhomas64@gmail.com>
2021-12-08 17:56:42 +0100

    add configuration for external connection

--- src/main/java/com/example/serverbabyboom/ServerBabyBoomApplication.java
+++ src/main/java/com/example/serverbabyboom/ServerBabyBoomApplication.java
@@ -2,12 +2,18 @@ package com.example.serverbabyboom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServerBabyBoomApplication {
+public class ServerBabyBoomApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerBabyBoomApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServerBabyBoomApplication.class);
+    }
 }
